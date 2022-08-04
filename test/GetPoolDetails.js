109da8b48b21541ea1b3649b030aca123b312813
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2022-08-04 20:05:59 +0530

    Tests: PremiumNFT, AdminRegistry, Staking

--- test/GetPoolDetails.js
+++ test/GetPoolDetails.js
@@ -1,23 +1,29 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-let Staking, StakingInstance;
+let accounts, owner, Staking, StakingInstance;
 describe("====>Staking<====", () => {
     beforeEach(async () => {
-        const accounts = await ethers.getSigners();
+        accounts = await ethers.getSigners();
         Staking = await ethers.getContractFactory("Staking");
         StakingInstance = await Staking.deploy();
-    })
+
+        owner = accounts[0];
+        user = accounts[1];
+        ownerAddress = await accounts[0].getAddress();
+    });
 
 
-    it('should get the Pool Status', async () => {
-        
+    it('should GET THE POOL STATUS', async () => {
+        const PoolStatus = await StakingInstance.pauseStatus();
+        expect(PoolStatus.toString()).to.equal('false');
+        console.log("The Pool's Status is: ", PoolStatus);
     })
 
     it('should get the Staking Period', async () => {
-        const staking_period = await StakingInstance.STAKING_PERIOD();
+        const staking_period = await StakingInstance.STAKING_MONTHS();
         expect(staking_period.toString()).to.equal('0');
-        console.log(staking_period);
+        console.log("The staking months is ", staking_period, "months, because it is not set yet");
     })
 
     // it('should get the Reward Composition', async () => {
@@ -33,16 +39,16 @@ describe("====>Staking<====", () => {
     // })
 
     it('should get the address of stakers', async () => {
-        await StakingInstance.stake();
+        console.log("The stakers address is: ", ownerAddress);
     })
-    
+
     it('should get the APY', async () => {
         const apy = await StakingInstance.APY();
         expect(apy.toString()).to.equal('0');
-        console.log(apy);
+        console.log("The APY of the stake is: ", apy, "%, because it is not set yet");
     })
 
-    it('should set the Reward Constant', async () => {
+    it('should get the Reward Constant', async () => {
             // Earned Amount = Initial Stake * [1 + r/n] ^ (n * y)
             // Since n = 1,
             // Total Earning = d * [1 + r] ^ y
@@ -63,19 +69,5 @@ describe("====>Staking<====", () => {
     expect(reward_constant.toString()).to.equal('11741');
     })
 
-
-    it('should get the Reward Constant', async () => {
-    let x = 0.90;
-    x += 1;
-    console.log("x", x);
-    let y = 0.25;
-    console.log("y", y);
-    
-    let val = (x**y).toPrecision(5);
-    console.log("exponent", val);
-    const reward_constant = val*10**4;
-    console.log('Reward Constant', reward_constant);
-    expect(reward_constant.toString()).to.equal('11741');
-    })
 
 })
--- test/Staking-test.test.ts
+++ test/Staking-test.test.ts
@@ -10,8 +10,6 @@ describe("===================>Staking<==================",function () {
     let owner: Signer;
     let ownerAddress: string; 
     let user: Signer;
-    let userAddress: string;
-    let userAddress2: string;
     let Staking: any;
     let staking: any; 
     let enoch: any;
@@ -33,135 +31,224 @@ describe("===================>Staking<==================",function () {
         StakeFactory = await ethers.getContractFactory("StakeFactory");
         Enoch = await ethers.getContractFactory('Enoch');
         PremiumNFT = await ethers.getContractFactory('PremiumNFT');
-        console.log("BeforeALL set");
     
       });
 
     this.beforeEach(async () => {
-        // [accounts] = await ethers.getSigners();
-        // owner = await ethers.getSigners();
-        // user = await ethers.getSigners();
-        // ownerAddress = await owner.address;
-        // userAddress = await user.address;
-        console.log("Setting beforeEach");
-
+        
         owner = accounts[0];
         user = accounts[1];
         ownerAddress = await accounts[0].getAddress();
-        userAddress = await accounts[1].getAddress();
-        // userAddress2 = await accounts[2].getAddress();
-
+        // userAddress = await accounts[1].getAddress();
+        
         adminRegistry = await AdminRegistry.deploy(ownerAddress);
         await adminRegistry.deployed();
-    console.log("AdminRegistry deployed at ", adminRegistry.address);
+    // console.log("AdminRegistry deployed at ", adminRegistry.address);
 
         staking = await Staking.deploy();
         await staking.deployed();
-    console.log("Staking deployed at ", staking.address);
+    // console.log("Staking deployed at ", staking.address);
 
         stakeFactory = await StakeFactory.deploy();    
         await stakeFactory.deployed();
-    console.log("Factory deployed at ", stakeFactory.address);
-        
-        enoch = await Enoch.deploy(adminRegistry.address);
-        await enoch.deployed();
-    console.log("Enoch deployed at ", enoch.address);
-        
-        premiumNFT = await PremiumNFT.deploy("Knight Templer Distillery", "KTD", adminRegistry.address);
-        await premiumNFT.deployed();
-    console.log("PremiumNFT deployed at ", premiumNFT.address);
-        
+    // console.log("Factory deployed at ", stakeFactory.address);
+    
+    enoch = await Enoch.deploy(adminRegistry.address);
+    await enoch.deployed();
+    // console.log("Enoch deployed at ", enoch.address);
+    
+    premiumNFT = await PremiumNFT.deploy("Knight Templer Distillery", "KTD", adminRegistry.address);
+    await premiumNFT.deployed();
+    // console.log("PremiumNFT deployed at ", premiumNFT.address);
+    
+    await stakeFactory.initialize(staking.address, adminRegistry.address);
     
 });
 
-    it('should initialize the stakeFactory', async () => {
-        await stakeFactory.initialize(staking.address, adminRegistry.address);
-        
-    });
 
-    it('Should set up staking contract', async () => {
+    it('INITIALIZING STAKING CONTRACT', async () => {
         let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
     
-    const receipt = await tx.wait();
-    let event = receipt.events?.find((event:any) => event.event === "StakeCreated");
-    console.log("event: ", event?.args);
+        const receipt = await tx.wait();
+        let event = receipt.events?.find((event:any) => event.event === "StakeCreated");
+        console.log("event: ", event?.args);
 
-    StakingInstance = await Staking.attach(event?.args?._stake);
-    console.log("Staking Instance", StakingInstance);
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        // console.log("Staking Instance", StakingInstance);
 
-    console.log("Let's fetch some public values from StakingInstance");
-
-    let rewardToken = await StakingInstance.rewardToken();
-    console.log("reward Token", rewardToken);
+        console.log("fetching some public values from StakingInstance");
+        let rewardToken = await StakingInstance.rewardToken();
+        console.log("reward Token", rewardToken);
         
     });
 
-    it('should stake', async () => {
-        let tx1 = await premiumNFT.connect(owner).mint(ownerAddress, 1);
-        let tx2 = await premiumNFT.connect(owner).approve(staking.address, 1);
-        let tx3 = await staking.connect(owner).setRewardConstant(11663);
+    it('should STAKE', async () => {
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt = await tx.wait();
+        let event = receipt.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
 
-        let tx = await staking.connect(owner).stake(ownerAddress, 1, 100);
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+        
+        console.log("\nHere are the steps involved in staking: \n");
+        
+        let tx1 = await premiumNFT.connect(owner).mint(ownerAddress);
+        console.log("Minting done");
+        
+        let tx2 = await premiumNFT.connect(owner).approve(stakingAddress, 1);
+        console.log("Approve done");
+
+        let tx3 = await StakingInstance.connect(owner).setRewardConstant(11663);
+        console.log("Reward Constant set");
+        
+        let tx4 = await StakingInstance.connect(owner).stake(ownerAddress, 1, 100);
         console.log("Staked!");
 
+        const receipt2 = await tx4.wait();
+        const event2 = receipt2.events?.find((event:any) => event.event === "NFTStaked");
+        console.log("\nHere are the events emitted out of 'NFTStaked' :\n");
+        
+        console.log("User staking the NFT is: ", event2?.args.user);
+        console.log("Token ID is: ", event2?.args.tokenId.toString());
+        console.log("Initial Balance is: ", event2?.args.initialBalance.toString());
+        console.log("Timestamp is: ", event2?.args.timestamp.toString());
+
     });
 
-    it('should claim reward', async () => {
-        console.log("The owner calling", ownerAddress);
-        let tx1 = await staking.setRewardConstant(11663);
-        console.log(tx1);
-        let tx = await staking._calculateRewards(100);
-        console.log(tx);
-        await staking.connect(owner).claimReward(ownerAddress, 1);
-        console.log("Reward claimed");
+    it('should CLAIM REWARD', async () => {
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt1 = await tx.wait();
+        let event = receipt1.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
+
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+
+        console.log("\nHere are the steps involved in staking: \n");
+        let tx1 = await premiumNFT.connect(owner).mint(ownerAddress);
+        console.log("mint done");
+        
+        let tx2 = await premiumNFT.connect(owner).approve(stakingAddress, 1);
+        console.log("approve done");
+
+        let tx3 = await StakingInstance.connect(owner).setRewardConstant(11663);
+        console.log("reward constant set");
+        
+        let tx4 = await StakingInstance.connect(owner).stake(ownerAddress, 1, 100);
+        console.log("Staked!");
+
+        let tx5 = await StakingInstance.connect(owner)._calculateRewards(100);
+        console.log("Rewards Calculated");
+
+        let tx6 = await StakingInstance.connect(owner).claimReward(ownerAddress, 1);
+        console.log("Rewards claimed!");
+
+        const receipt2 = await tx6.wait();
+        const event2 = receipt2.events?.find((event:any) => event.event === "RewardsClaimed");
+        console.log("\nHere are the events emitted out of 'RewardsClaimed' :\n");
+        console.log("User is: ", event2?.args._user.toString());
+        console.log("Staked Token ID is: ", event2?.args._stakedTokenId.toString());
+        console.log("Reward Amount is: ", event2?.args._rewardAmount.toString());
+        console.log("Timestamp is: ", event2?.args._timestamp.toString());
+
     });
 
-    it('should calculate reward', async () => {
-        let tx1 = await staking.setRewardConstant(11663);
-        let tx = await staking._calculateRewards(100);
-        expect(tx).to.equal('1166300', '');
+    it('should CALCULATE REWARD', async () => {
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt1 = await tx.wait();
+        let event = receipt1.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
 
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+
+        console.log("\nHere are the steps involved in calculating the Rewards: \n");
+        let tx1 = await StakingInstance.connect(owner).setRewardConstant(11663);
+        console.log("Reward Constant set");
+
+        let tx2 = await StakingInstance.connect(owner)._calculateRewards(100);
+        console.log("Rewards Calculated");
+        const receipt2 = await tx2.wait();
+        const event2 = receipt2.events?.find((event:any) => event.event === "MsgSender");
+        console.log("\nHere are the events emitted out of 'MsgSender' :\n");
+        console.log("Sender's address is: ", event2?.args._sender.toString());
     });
     
-    it('should set reward constant', async () => {
-        let x = 0.90;
-        x += 1; 
-        console.log("x", x);
-        let y = 0.25;
-        console.log("y", y);
+    it('should SET REWARD CONSTANT', async () => {
+//         // let x = 0.90;
+//         // x += 1; 
+//         // console.log("x", x);
+//         // let y = 0.25;
+//         // console.log("y", y);
 
-        let val:any = (x**y).toPrecision(5);
-        console.log("exponent", val);
+//         // let val:any = (x**y).toPrecision(5);
+//         // console.log("exponent", val);
 
-        const reward_constant = val*10**4;
-        console.log('Reward Constant', reward_constant);
-        expect(await staking.setRewardConstant(11663)).to.equal('11663');
+//         // const reward_constant = val*10**4;
+//         // console.log('Reward Constant', reward_constant);
+
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt1 = await tx.wait();
+        let event = receipt1.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
+
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+
+        let tx1 = await StakingInstance.connect(owner).setRewardConstant(11663);
+        console.log("Reward Constant set");
+        // console.log(tx1);
+        // expect(tx1.toString()).to.equal('11663');
 
 });
 
-    it('should get staked info', async () => {
+    it('should GET STAKED INFO', async () => {
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt = await tx.wait();
+        let event = receipt.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
 
-        let tx1 = await premiumNFT.connect(owner).mint(ownerAddress, 1);
-        let tx2 = await premiumNFT.connect(owner).approve(staking.address, 1);
-        let tx3 = await staking.connect(owner).setRewardConstant(11663);
-
-        let tx = await staking.connect(owner).stake(ownerAddress, 1, 100);
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+        
+        console.log("\nHere are the steps involved in Staking: \n");
+        let tx1 = await premiumNFT.connect(owner).mint(ownerAddress);
+        console.log("mint done");
+        
+        let tx2 = await premiumNFT.connect(owner).approve(stakingAddress, 1);
+        console.log("approve done");
+        let tx3 = await StakingInstance.connect(owner).setRewardConstant(11663);
+        console.log("reward constant set");
+        
+        let tx4 = await StakingInstance.connect(owner).stake(ownerAddress, 1, 100);
         console.log("Staked!");
 
         console.log("\nUser Stake Info\n");
-        let tx4 = await staking.connect(owner).getStakedInfo(ownerAddress, 1);
-        console.log(tx4);
+        let tx5 = await StakingInstance.connect(owner).getStakedInfo(ownerAddress, 1);
+        console.log("stakingTimestamp, stakedAmount, totalClaimableRewards, claimedRewards, rewardInstallment, lastWithdrawalTime is: ", tx5.toString(), "respectively.");
         
     });
 
-    it('should get pending rewards info', async () => {
-        let tx = await staking.connect(owner).getStakedInfo(ownerAddress, 1);
-        console.log(tx);
-        const Reward_info1 = await staking.getStakedInfo.totalClaimableRewards;
-        const Reward_info2 = await staking.getStakedInfo.claimedRewards;
-        const Reward_info = Reward_info1 - Reward_info2;
-        await staking.connect(owner).getPendingRewardsInfo(ownerAddress, 1) == Reward_info;
-        expect(Reward_info).to.equal(0);
+    it('should GET PENDING REWARDS INFO', async () => {
+        let tx = await stakeFactory.connect(owner).setupStakeContract(premiumNFT.address, enoch.address, 90, 3, adminRegistry.address);
+    
+        const receipt = await tx.wait();
+        let event = receipt.events?.find((event:any) => event.event === "StakeCreated");
+        // console.log("event: ", event?.args);
+    
+        StakingInstance = await Staking.attach(event?.args?._stake);
+        let stakingAddress: any = event?.args?._stake;
+        
+        let tx1 = await StakingInstance.connect(owner).getPendingRewardsInfo(ownerAddress, 1);
+        console.log("Pending reward INfo is: ",tx1.toString());
+        
+        expect(tx1).to.equal(0);
     });
     
-})
+});
