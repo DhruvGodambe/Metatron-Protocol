34508d5ade39c8816783a2bb89c1c97d78337c88
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2022-09-28 15:35:08 +0530

    Interface Enoch1 contract in wormhole script

--- scripts/wormholeTransfer.ts
+++ scripts/wormholeTransfer.ts
@@ -1,13 +1,25 @@
-const 
+const hre = require("hardhat");
+import '@nomiclabs/hardhat-ethers';
+import {ethers} from "hardhat";
 
 const main = async () => {
     
-    .attach(contract_address)
+  const Enoch = await ethers.getContractFactory("Enoch1");
+  const enoch = await Enoch.attach(
+    "0xC7cB566FB6f662E4543E28D3DeADdE2a3b9637Eb" // The deployed contract address
+  );
+  
+  // Now you can call functions of the contract
+  //await enoch._mint();
+  //await enoch._owner();
 
     // need a contract to interact with ERC20
     // ENOCH interface
 
     // TRANSFER ENOCH -> to my address 
+    await enoch.transfer("0x259989150c6302D5A7AeEc4DA49ABfe1464C58fE", 50);
+    console.log("Transferred ENOCH to the given address");
+    
 
     // function -> approve to token bridge
     // function -> transfer of tokens
