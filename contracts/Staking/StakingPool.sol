4137fdd0c16c0ecf9eda2f60ee55eeacc4dedbde
Ishrani Rajiv <rajivishrani@gmail.com>
2022-12-06 16:25:24 +0530

    updated: StakingPool- stake

--- contracts/Staking/StakingPool.sol
+++ contracts/Staking/StakingPool.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.10;
 
 import "../Tokens/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 
 error StakingPool__StakeFailed(uint256 required);
 error StakingPool__WithdrawFailed(uint256 required);
 
-contract Pool {
+contract StakingPool {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private positionId;
 
     struct UserInfo {
         uint32 positionId;
@@ -16,42 +21,43 @@ contract Pool {
         
     }
 
-    UserInfo[] public userInfo;
+    // UserInfo[] public userInfo;
 
     IERC20 public immutable stakingToken; //RJToken
     IERC20 public immutable rewardToken; //RWDToken
 
     mapping(address => UserInfo[]) public userPositions; 
     mapping(address => uint256) public balanceOf; //RJToken balance of a user
-    // mapping(address => uint256) public startTime;
-    // mapping (address => uint256) public endTime; //when user unstakes time stores here
     mapping(address => uint256) public rewards; // User address => rewards already claimed
 
-
-    // uint256 public duration;
     // use openzepplin counter contract
-    uint32 public positionIdCounter;
+    // uint32 public positionIdCounter;
     uint256 public APY;
-    uint256 public stakingTimeConstant; // staking time constant dynamic
+    uint256 public constant PRECISION_CONSTANT = 10000;
+    uint256 public stakingTimeConstant; 
 
     event Staked(
-        address indexed user,
-        uint256 indexed amount,
-        uint256 timestamp
+        address indexed _user,
+        uint256 indexed _amount,
+        uint256 _timestamp,
+        uint256 indexed positionId
     );
 
     event Unstake(
-        address indexed user,
-        uint256 indexed amount,
-        uint256 timestamp
+        address indexed _user,
+        uint256 indexed _amount,
+        uint256 _timestamp
     );
+
+    event RewardsClaimed(address indexed _user, uint256 _rewardAmount, uint256 _timestamp);
+
 
     constructor(address _stakingToken, address _rewardToken, uint256 _APY, uint256 _stakingTimeConstant) {
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken); //RJToken
         rewardToken = IERC20(_rewardToken); //RWDToken
         APY = _APY;
-        stakingTimeConstant = _stakingTimeConstant;
+        stakingTimeConstant = _stakingTimeConstant; // e.g. 2HR = 2*60*60 = 7200 sec
     }
 
     modifier onlyOwner() {
@@ -63,31 +69,37 @@ contract Pool {
         require(_amount > 0, "amount cannot be 0");
 
         // check for approval         
-        require(stakingToken.allowance(msg.sender, address(this)) >= _amount, "Staking Contract is not approved for this Token or approved amount is not equal to given amount" );        
+        require(stakingToken.allowance(msg.sender, address(this)) >= _amount, "Staking Contract is not approved for this Token or approved amount is not equal to given amount" );  
 
-        // balanceOf[msg.sender] += _amount;
-        // startTime[msg.sender] = block.timestamp;
-        // userInfo.push(UserInfo(msg.sender, block.timestamp, _amount));
-        // 1. get users existing position and store it an array
-        // 2. condition if user position empty creatae new array or else push new position in the user position array
-        // set position id
-        //3. update user position for perticular user
-        // increment position id counter
+        if(userPositions[msg.sender].positionId != 0 && msg.sender != address(0)) {
+            // keep track of how much this user has staked
+            uint256 position = userPositions[msg.sender].positionId.increment();
+            userPositions[msg.sender].positionId = position ; 
+            userPositions[msg.sender].startTime = block.timestamp;
+            userPositions[msg.sender].tokenStakedAmount += _amount;
+        }    
+
+
         bool success = stakingToken.transferFrom(
             msg.sender,
             address(this),
             _amount
         );
-        // require(success, "Failed to Stake");
         if (!success) {
             revert StakingPool__StakeFailed({required: _amount});
         }
 
-        emit Staked(msg.sender, _amount, block.timestamp);
+        // 1. get users existing position and store it an array
+        // 2. condition if user position empty creatae new array or else push new position in the user position array
+        // set position id
+        //3. update user position for perticular user
+        // increment position id counter
+
+        emit Staked(msg.sender, _amount, block.timestamp, position);
     }
 
     function withdraw() external {
-        uint balance = balanceOf[msg.sender]; // create position for balances
+        uint balance = userPositions[msg.sender].tokenStakedAmount; // create position for balances
         require(balance > 0, "amount = 0");
 
         require((block.timestamp - startTime[msg.sender]) >= stakingTimeConstant, "User cannot claim rewards before due time!");
@@ -123,7 +135,12 @@ contract Pool {
             revert StakingPool__WithdrawFailed({required: reward});
         }
     }
+
     // get all position of the given user
+    function getPositions (address _account) public view returns (uint256) {
+        return 
+    }
+
     // user position of _address return array of all user position
     //earned reward will take _address and positionId and return reward of particular postionId
 
