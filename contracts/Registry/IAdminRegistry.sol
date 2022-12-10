2580bb909c3df9a58f6b505b54d3a9200c28b0f5
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2022-12-10 23:25:43 +0530

    IAdminRegistry updated

--- contracts/Registry/IAdminRegistry.sol
+++ contracts/Registry/IAdminRegistry.sol
@@ -8,12 +8,19 @@ interface IAdminRegistry {
 
     function getRoleMemberCount(bytes32 role) external view returns (uint256);
 
-    function getRoleMembers()
+    function getAdminRoleMembers()
+        external
+        view
+        returns (uint256, address[] memory);
+    
+    function getTreasuryRoleMembers()
         external
         view
         returns (uint256, address[] memory);
 
     function isAdmin(address account) external view returns (bool);
+    
+    function isTreasury(address account) external view returns (bool);
 
     function addAdmin(address account) external;
 
