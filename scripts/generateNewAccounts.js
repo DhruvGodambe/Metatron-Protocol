c96cdfa16285f5dd2e31964fe309533b4359f9bd
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2022-12-14 20:02:20 +0530

    deployed contracts with new admin

--- scripts/generateNewAccounts.js
+++ scripts/generateNewAccounts.js
@@ -4,14 +4,14 @@ const { writeFileSync } = require("fs");
 async function main() {
   let result = {};
 
-  for (let i = 0; i < 4; i++) {
-    let wallet = ethers.Wallet.createRandom();
-    result[`account_${i + 1}`] = {};
-    result[`account_${i + 1}`].address = wallet.address;
-    result[`account_${i + 1}`].mnemonic = wallet.mnemonic.phrase;
-    result[`account_${i + 1}`].privateKey = wallet.privateKey;
-  }
 
+    let wallet = ethers.Wallet.createRandom();
+    result[`account_${1}`] = {};
+    result[`account_${1}`].address = wallet.address;
+    result[`account_${1}`].mnemonic = wallet.mnemonic.phrase;
+    result[`account_${1}`].privateKey = wallet.privateKey;
+
+    console.log(result);
   //    writing accounts to the accounts.json file
   //   writeFileSync("accounts.json", JSON.stringify(result, null, 2));
 }
