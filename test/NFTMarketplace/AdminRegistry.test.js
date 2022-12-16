c4f3dd7539f558f3271153e64853d4ce877b9073
Ishrani Rajiv <rajivishrani@gmail.com>
2022-12-16 13:32:23 +0530

    solved error and refactored

--- test/NFTMarketplace/AdminRegistry.test.js
+++ test/NFTMarketplace/AdminRegistry.test.js
@@ -29,45 +29,52 @@ describe("Admin Registry", () => {
     it("should set constructor addresses correctly", async () => {
         const admin = ownerAddress;
         const treasurer = treasuryAddress;
+
         assert.equal(admin, ownerAddress);
         assert.equal(treasurer, treasuryAddress);
     });
 
     it("Should Check user is a Admin", async () => {
-        expect(await adminRegistry.connect(owner).isAdmin(ownerAddress)).to.be
-            .true;
+        const tx = await adminRegistry.connect(owner).isAdmin(ownerAddress);
+
+        expect(tx).to.be.true;
     });
 
     it("Should Check user is a Treasurer", async () => {
-        expect(await adminRegistry.connect(owner).isTreasury(treasuryAddress))
-            .to.be.true;
+        const tx = await adminRegistry
+            .connect(owner)
+            .isTreasury(treasuryAddress);
+
+        expect(tx).to.be.true;
     });
-    it("Should only admin can add new admin", async () => {
-        expect(
-            await adminRegistry.connect(owner).addAdmin(user2Address)
-        ).to.be.revertedWith("You are not a Admin!");
+
+    it("Only admin can add new admin", async () => {
+        const tx = await adminRegistry.connect(owner).addAdmin(user2Address);
+
+        expect(tx).to.be.revertedWith("You are not a Admin!");
     });
+
     // it("User can leave their role", async () => {});
     it("Only Admin can remove other admin role", async () => {
         const tx = await adminRegistry.connect(owner).addAdmin(userAddress);
-        expect(
-            await adminRegistry.connect(owner).removeAdmin(userAddress)
-        ).to.be.revertedWith("You are not a Admin!");
+
+        expect(tx).to.be.revertedWith("You are not a Admin!");
     });
+
     it("Returns Admin Role Members", async () => {
         const numbOfAdmins = 1;
+        const tx = await adminRegistry.connect(owner).getAdminRoleMembers();
 
-        assert.equal(await adminRegistry.connect(owner).getAdminRoleMembers(), [
-            numbOfAdmins.toString(),
-            [ownerAddress],
-        ]);
+        expect(tx).to.deep.equal([numbOfAdmins.toString(), [ownerAddress]]);
     });
+
     it("Should return Treasury Role Members", async () => {
         const numbOfTreasurer = 1;
-        console.log(numbOfTreasurer)
-        assert.equal(
-            await adminRegistry.connect(owner).getTreasuryRoleMembers(),
-            [1, [treasuryAddress]]
-        );
+        const tx = await adminRegistry.connect(owner).getTreasuryRoleMembers();
+
+        expect(tx).to.deep.equal([
+            numbOfTreasurer.toString(),
+            [treasuryAddress],
+        ]);
     });
 });
