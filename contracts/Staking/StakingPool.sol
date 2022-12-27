2787cbefdf10c5ac4829cd3ada1550f5fe962884
Ishrani Rajiv <rajivishrani@gmail.com>
2022-12-27 22:10:46 +0530

    added Pausable and ReentrancyGuard

--- contracts/Staking/StakingPool.sol
+++ contracts/Staking/StakingPool.sol
@@ -2,28 +2,31 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract StakingPool {
+contract StakingPool is Ownable, Pausable, ReentrancyGuard {
     IERC20 public immutable stakingToken; //RJToken
     IERC20 public immutable rewardToken; //RWDToken
 
-    uint256 public minDuration = 60;
+    uint256 public minDuration = 60; // 3 Month
 
     // Annual reward percentage
-    uint256 public rewardPercentage = 20; //20%
+    uint256 public rewardPercentage = 2000; //20%
+
+    uint256 public constant PRECISION_CONSTANT = 10000;
 
     uint256 public constant YEAR = 31536000; //365days * 24hours * 60min * 60sec
 
-    uint256 public totalStakedAmount;
-
-    struct StakedAmount {
+    struct StakePosition {
         uint256 amount;
         uint256 duration;
+        //bool redeemed;
     }
 
     //user address to an array of their staked amounts and durations
-    mapping(address => StakedAmount[]) public stakes;
+    mapping(address => StakePosition[]) public stakes;
 
     event Staked(
         address indexed _user,
@@ -43,17 +46,15 @@ contract StakingPool {
         rewardToken = IERC20(_rewardToken); //RWDToken
     }
 
-    function stake(uint256 _amount) public {
+    function stake(uint256 _amount) public whenNotPaused nonReentrant {
         require(_amount > 0, "amount cannot be 0");
         require(
             stakingToken.allowance(msg.sender, address(this)) >= _amount,
             "Staking Contract is not approved for this Token or approved amount is not equal to given amount"
         );
 
-        totalStakedAmount += _amount;
-
         // Create a new staked amount and duration for the user
-        StakedAmount memory newStake = StakedAmount(_amount, block.timestamp);
+        StakePosition memory newStake = StakePosition(_amount, block.timestamp);
         stakes[msg.sender].push(newStake);
 
         stakingToken.transferFrom(msg.sender, address(this), _amount);
@@ -61,20 +62,20 @@ contract StakingPool {
         emit Staked(msg.sender, _amount, block.timestamp);
     }
 
-    function Unstake(uint256 _positionIndex) public {
+    function Unstake(uint256 _positionIndex) public nonReentrant {
         require(_positionIndex < stakes[msg.sender].length, "Invalid position");
+        //check struct bool here
+        //require redeem should be false to unstake user token
 
         // Get the staked amount and duration for the specified position
-        StakedAmount memory stake = stakes[msg.sender][_positionIndex];
+        StakePosition memory stake = stakes[msg.sender][_positionIndex];
         uint256 amount = stake.amount;
         uint256 duration = block.timestamp - stake.duration;
 
         uint256 rewardAmount = ((amount * rewardPercentage * duration) /
-            (100 * YEAR));
+            (PRECISION_CONSTANT * YEAR));
 
         require(duration >= minDuration, "Minimum staking duration not met");
-
-        totalStakedAmount -= amount;
 
         stakingToken.transfer(msg.sender, amount);
         rewardToken.transfer(msg.sender, rewardAmount);
@@ -84,6 +85,7 @@ contract StakingPool {
             stakes[msg.sender].length - 1
         ];
         delete stakes[msg.sender][stakes[msg.sender].length - 1];
+        //make redeem status true after complition
 
         emit RewardsClaimed(msg.sender, amount, rewardAmount, block.timestamp);
     }
@@ -92,17 +94,25 @@ contract StakingPool {
         require(_positionIndex < stakes[msg.sender].length, "Invalid position");
 
         // Get the staked amount and duration for the specified position
-        StakedAmount memory getStake = stakes[msg.sender][_positionIndex];
+        StakePosition memory getStake = stakes[msg.sender][_positionIndex];
         uint256 amount = getStake.amount;
         uint256 duration = block.timestamp - getStake.duration;
 
         uint256 rewardAmount = ((amount * rewardPercentage * duration) /
-            (100 * YEAR));
+            (PRECISION_CONSTANT * YEAR));
 
         return rewardAmount;
     }
+
+    function getTotalStakedAmount() public view returns (uint256) {
+        return stakingToken.balanceOf(address(this));
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
-//0.03805175038051750380517503805175
-//380517.5038051750380517503805175
-//31536000
-//15768000
