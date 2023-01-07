ec0c0089e4c50db253811b65505baa8b0de8ce35
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2023-01-07 14:56:26 +0530

    updated deploymentMsgSender

--- scripts/UUPS/deploymentMsgSender.js
+++ scripts/UUPS/deploymentMsgSender.js
@@ -8,8 +8,10 @@ const MintingFactoryProxyabi = require('../../artifacts/contracts/NFTMarketplace
 const MintingFactoryabi = require('../../artifacts/contracts/NFTMarketplace/MintingAndStorage/MintingFactory/MintingFactory.sol/MintingFactory.json');
 const enochTokenabi = require('../../artifacts/contracts/Tokens/Enoch.sol/Enoch.json');
 
-const enochTokenAddress = "0x1d3702D92A8c9Fd3fcD0220e13e5A47f56375d7b";
-const adminAddress = "0xCB61f141D37C320B4357173ec28Af37A5E09d949";
+const enochTokenAddress = Book.ENOCHTOKEN_ADDRESS;
+const adminAddress = Book.ADMIN_ADDRESS;
+const adminRegistryAddress = Book.ADMIN_REGISTRY_ADDRESS;
+const treasuryAddress = Book.TREASURY_ADDRESS;
 NFT_COLLECTION = "0x1669cBB15538A4B9dfE659A1601EEB9f24Cd0Cc9";
 let Exchange_V1_ADDRESS;
 let exchange1967PROXY;
@@ -17,47 +19,6 @@ let EXCHANGE_V1_Proxy;
 let MintingFactory_V1_ADDRESS;
 let mintingFactory1967PROXY;
 let MINTINGfACTORY_V1Proxy;
-
-const deployExchangeCoreV1 = async () => {
-
-    const exchangeCore = await hre.ethers.getContractFactory("ExchangeCore");
-    console.log("Deploying ExchangeCore...");
-    const ExchangeCoreV1 = await exchangeCore.deploy();
-    await ExchangeCoreV1.deployed();
-    console.log("ExchangeCore deployed");
-    console.log("ExchangeCoreV1 Contract deployed to: ", ExchangeCoreV1.address);
-    Exchange_V1_ADDRESS = ExchangeCoreV1.address;
-
-}
-
-const deployExchangeCoreProxy = async () => {
-
-  const encodedData = "0xc0c53b8b0000000000000000000000009725caa9dc09884abedf8ed4c00703554027fb4900000000000000000000000069d260289d8422496f0bd50a17d6ed6b98f1851e000000000000000000000000404dbbbd516d101b41ce1671c9e5d0766272d047";
-
-  const exchangeCoreProxy = await hre.ethers.getContractFactory("ExchangeCoreProxy");
-  console.log("Deploying ExchangeCore Proxy...");
-  const proxy = await exchangeCoreProxy.deploy
-  (
-    Exchange_V1_ADDRESS,
-    encodedData 
-  );
-
-  await proxy.deployed();
-  exchange1967PROXY = proxy.address;
-  console.log("Exchange Core 1967 PROXY address: ", exchange1967PROXY);
-
-}
-
-const ExchangeLogicV1Proxy = async () => {
-
-    const accounts = await ethers.getSigners();
-  const admin  = accounts[0];
-  const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/OW3K8LQl3oZeZLxuOTzgbRkFsEBkThgA");
-
-  const proxy = new ethers.Contract(exchange1967PROXY, ExchangeCoreabi.abi, provider);
-  EXCHANGE_V1_Proxy = proxy.address;
-
-}
 
 
 const deployMintingFactoryV1 = async () => {
@@ -92,6 +53,90 @@ const deployMintingFactoryProxy = async () => {
 
 const MintingFactoryLogicV1Proxy = async () => {
 
+  const accounts = await ethers.getSigners();
+  const admin  = accounts[0];
+  const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/OW3K8LQl3oZeZLxuOTzgbRkFsEBkThgA");
+
+  const proxy = new ethers.Contract(mintingFactory1967PROXY, MintingFactoryabi.abi, provider);
+  MINTINGfACTORY_V1Proxy = proxy.address;
+
+}
+
+
+const deployExchangeCoreV1 = async () => {
+
+    const exchangeCore = await hre.ethers.getContractFactory("ExchangeCore");
+    console.log("Deploying ExchangeCore...");
+    const ExchangeCoreV1 = await exchangeCore.deploy();
+    await ExchangeCoreV1.deployed();
+    console.log("ExchangeCore deployed");
+    console.log("ExchangeCoreV1 Contract deployed to: ", ExchangeCoreV1.address);
+    Exchange_V1_ADDRESS = ExchangeCoreV1.address;
+
+}
+
+const deployExchangeCoreProxy = async () => {
+
+    const constructorABI = [{
+		"inputs": [
+		  {
+			"internalType": "contract IMintingFactory",
+			"name": "_mintingFactory",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "_adminRegistry",
+			"type": "address"
+		  },
+		  {
+			"internalType": "address",
+			"name": "_treasury",
+			"type": "address"
+		  }
+		],
+		"name": "initialize",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	  }];
+
+    const args = [MINTINGfACTORY_V1Proxy, 
+        adminRegistryAddress,
+        treasuryAddress
+    ]
+
+    let interface= new ethers.utils.Interface(constructorABI);
+    const encodedData = interface.encodeFunctionData("initialize", args);
+    console.log("encodedData : ", encodedData);
+
+  const exchangeCoreProxy = await hre.ethers.getContractFactory("ExchangeCoreProxy");
+  console.log("Deploying ExchangeCore Proxy...");
+  const proxy = await exchangeCoreProxy.deploy
+  (
+    Exchange_V1_ADDRESS,
+    encodedData 
+  );
+
+  await proxy.deployed();
+  exchange1967PROXY = proxy.address;
+  console.log("Exchange Core 1967 PROXY address: ", exchange1967PROXY);
+
+}
+
+const ExchangeLogicV1Proxy = async () => {
+
+    const accounts = await ethers.getSigners();
+  const admin  = accounts[0];
+  const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/OW3K8LQl3oZeZLxuOTzgbRkFsEBkThgA");
+
+  const proxy = new ethers.Contract(exchange1967PROXY, ExchangeCoreabi.abi, provider);
+  EXCHANGE_V1_Proxy = proxy.address;
+
+}
+
+const SettingExchangeinMintingFactorylogicV1Proxy = async () => {
+
     const accounts = await ethers.getSigners();
   const admin  = accounts[0];
   const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/OW3K8LQl3oZeZLxuOTzgbRkFsEBkThgA");
@@ -106,17 +151,19 @@ const MintingFactoryLogicV1Proxy = async () => {
     const receipt1 = await tx1.wait();
     let event = receipt1.events?.find((event) => event.event === "ExchangeAddressChanged");
     console.log("Updated Exchange Address is : ", event?.args.newExchange);
+
 }
 
 
 
 const main = async () => {
-    await deployExchangeCoreV1();
-    await deployExchangeCoreProxy();
-    await ExchangeLogicV1Proxy();
     await deployMintingFactoryV1();
     await deployMintingFactoryProxy();
     await MintingFactoryLogicV1Proxy();
+    await deployExchangeCoreV1();
+    await deployExchangeCoreProxy();
+    await ExchangeLogicV1Proxy();
+    await SettingExchangeinMintingFactorylogicV1Proxy();
 }
 
 
