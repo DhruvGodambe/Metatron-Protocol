aa4ffa27f916042eb6bc0e8fc569fb61780d903b
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2023-01-20 14:32:13 +0530

    gift card updated

--- contracts/GiftCard/GiftCard.sol
+++ contracts/GiftCard/GiftCard.sol
@@ -4,69 +4,79 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./../NFTMarketplace/Interface/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract GiftCard {
+contract GiftCard is
+    Initializable
+{
 
     using SafeMath for uint256;
-    address public owner;
+    address public factory;
+    address public user;
     address public spender;
-    address public tokenAddress;
 
+    mapping(address => uint256) public tokenBalanceInDataCard; //token's address
 
-    mapping(address => mapping(address => uint256)) public balance;
+    event SingleTokenTransferred(address _to, uint256 _amount);
+    event MultiTokenTransferred(address _to, address tokens, uint256 _amount);
 
-    constructor(address _tokenAddress) public {
-        owner = msg.sender;
+    function initialize(address _user) external initializer {
+        factory = msg.sender;
+        user = _user;
         spender = address(this);
-        tokenAddress = _tokenAddress;
     console.log("msg.sender is : ", msg.sender);
     console.log("address(this) is : ", address(this));
     }
 
+    modifier onlyUser() {
+        require(msg.sender == user,
+        "Only User can call this!");
+        _;
+    }
 
-    function topUp(uint256 _amount) public {
-        require(msg.sender == owner, "Only the owner can top up gift cards");
+
+    function topUp(address _token, address _from, uint256 _amount) public onlyUser {
         console.log("msg.sender in topUp is : ", msg.sender);
-        // require(IERC20(tokenAddress).allowance(owner, spender) >= _amount , "Insufficient allowance");
-        IERC20(tokenAddress).transferFrom(owner, spender, _amount);
-        balance[spender][tokenAddress] = balance[spender][tokenAddress].add(_amount);
+
+        IERC20(_token).transferFrom(_from, spender, _amount);
+        tokenBalanceInDataCard[_token] = tokenBalanceInDataCard[_token].add(_amount);
     }
 
 
-    function payNow
+    function singleTokenTransfer
     (
-        address from, address to, 
-        address[] memory tokens,
-        uint256[] memory amounts
-    ) public returns(bool) {
-
-        for (uint i = 0; i < tokens.length-1; i++) {
-            bool success = IERC20(tokens[i]).transferFrom(from, to, amounts[i]);
-        
-        return success;
-        }
-    }
-
-    function payNowBatchTransfer
-    (
-        address token1, address token2, 
-        address from, address to, 
-        uint256 amount
-    ) public returns (bool, bool) 
+        address _token,
+        address _to,
+        uint256 _amount
+    ) public onlyUser returns (bool)
     {
-        bool success1 = IERC20(token1).transferFrom(from, to, amount);
-        bool success2 = IERC20(token2).transferFrom(from, to, amount);
-        return (success1, success2);
+        IERC20(_token).transfer(_to, _amount);
+        tokenBalanceInDataCard[_token] = tokenBalanceInDataCard[_token].sub(_amount);
+
+        emit SingleTokenTransferred(_to, _amount);
+        return true;
     }
 
-    function redeem(address _token) public {
-        require(balance[address(this)][_token] > 0, "Gift card has no balance in the given token");
-        IERC20(_token).transfer(msg.sender, balance[address(this)][_token]);
-        balance[address(this)][_token] = 0;
+
+    function multiTokenTransfer
+    (
+        address[] memory _tokens,
+        address _to, 
+        uint256[] memory _amounts
+    ) public onlyUser returns(bool)
+    {
+
+        for (uint i = 0; i < _tokens.length; i++) {
+            IERC20(_tokens[i]).transfer(_to, _amounts[i]);
+        tokenBalanceInDataCard[_tokens[i]] = tokenBalanceInDataCard[_tokens[i]].sub(_amounts[i]);
+        emit MultiTokenTransferred(_to, _tokens[i], _amounts[i]);
+        }
+        return true;
     }
 
-    function getBalance(address _token) public view returns (uint256) {
-        return balance[spender][_token];
+
+    function getBalance(address _token, address _account) public view returns (uint256) {
+        return IERC20(_token).balanceOf(_account);
     }
 
 }
