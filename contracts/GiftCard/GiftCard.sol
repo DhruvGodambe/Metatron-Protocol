3b6a8576f569de2de88f07c6e12d3f9e5ce28c5b
ankit kathayat <ankit.kathayat@elementzerolabs.com>
2023-01-21 17:10:10 +0530

    removed top up function

--- contracts/GiftCard/GiftCard.sol
+++ contracts/GiftCard/GiftCard.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./../NFTMarketplace/Interface/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./../NFTMarketplace/Interface/IERC20.sol";
+import "./../Registry/IAdminRegistry.sol";
 
 contract GiftCard is
     Initializable
@@ -12,34 +13,25 @@ contract GiftCard is
 
     using SafeMath for uint256;
     address public factory;
-    address public user;
-    address public spender;
+    address public adminRegistry;
 
-    mapping(address => uint256) public tokenBalanceInDataCard; //token's address
 
     event SingleTokenTransferred(address _to, uint256 _amount);
     event MultiTokenTransferred(address _to, address tokens, uint256 _amount);
 
-    function initialize(address _user) external initializer {
+    function initialize(address _adminRegistry) external initializer {
         factory = msg.sender;
-        user = _user;
-        spender = address(this);
+        adminRegistry = _adminRegistry;
     console.log("msg.sender is : ", msg.sender);
     console.log("address(this) is : ", address(this));
     }
 
-    modifier onlyUser() {
-        require(msg.sender == user,
-        "Only User can call this!");
+    modifier onlyAdmin() {
+        require(
+            IAdminRegistry(adminRegistry).isAdmin(msg.sender),
+            "Only Admin can call this!"
+        );
         _;
-    }
-
-
-    function topUp(address _token, address _from, uint256 _amount) public onlyUser {
-        console.log("msg.sender in topUp is : ", msg.sender);
-
-        IERC20(_token).transferFrom(_from, spender, _amount);
-        tokenBalanceInDataCard[_token] = tokenBalanceInDataCard[_token].add(_amount);
     }
 
 
@@ -48,10 +40,9 @@ contract GiftCard is
         address _token,
         address _to,
         uint256 _amount
-    ) public onlyUser returns (bool)
+    ) public onlyAdmin returns (bool)
     {
         IERC20(_token).transfer(_to, _amount);
-        tokenBalanceInDataCard[_token] = tokenBalanceInDataCard[_token].sub(_amount);
 
         emit SingleTokenTransferred(_to, _amount);
         return true;
@@ -63,20 +54,38 @@ contract GiftCard is
         address[] memory _tokens,
         address _to, 
         uint256[] memory _amounts
-    ) public onlyUser returns(bool)
+    ) public onlyAdmin returns(bool)
     {
 
         for (uint i = 0; i < _tokens.length; i++) {
             IERC20(_tokens[i]).transfer(_to, _amounts[i]);
-        tokenBalanceInDataCard[_tokens[i]] = tokenBalanceInDataCard[_tokens[i]].sub(_amounts[i]);
+
         emit MultiTokenTransferred(_to, _tokens[i], _amounts[i]);
         }
         return true;
     }
 
 
-    function getBalance(address _token, address _account) public view returns (uint256) {
-        return IERC20(_token).balanceOf(_account);
+    function getBalance(address _token) public view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
+    }
+
+    function getSingleTokenAllowance(address _token, address _tokenOwner) public view returns (uint256) {
+        return IERC20(_token).allowance(_tokenOwner, address(this));
+    }
+
+    function getMultiTokenAllowance
+    (
+        address[] memory _tokens,
+        address[] memory _tokenOwners
+    ) public view returns(uint[] memory)
+    {
+
+        uint[] memory myArray = new uint[](_tokens.length);
+        for (uint i = 0; i < _tokens.length; i++) {
+            myArray[i] = IERC20(_tokens[i]).allowance(_tokenOwners[i], address(this));
+        }
+    return myArray;
     }
 
 }
